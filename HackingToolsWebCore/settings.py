2960b519b8ef471ac7ea0b9052d1282294168b43
Rubén Barroso Blázquez <rubenbarroso16@gmail.com>
2022-06-11 00:08:02 +0200

    fix minor error in runtime

--- HackingToolsWebCore/settings.py
+++ HackingToolsWebCore/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.HomePage',
-    'apps.webScraping',
+    'apps.WebScraping',
     'apps.apiSniffer',
 ]
 
