7582fe2e35243947e5e24418c7068aa2a27d3181
Rubén Barroso Blázquez <rubenbarroso16@gmail.com>
2022-06-30 20:37:20 +0200

    New : merge webScraping functionality

--- HackingToolsWeb/DB/DatabaseFactory/Enum/DatabaseTypesEnum.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-
-class DATABASE_TYPES(Enum):
-    MYSQL = 'mysql'
-    MONGO_DB = 'mongo_db'
-    ELASTICSEARCH = 'elasticsearch'
---
+++ static/resources/script.js
@@ -0,0 +1,5 @@
+const backendUrl = JSON.parse(document.getElementById('WEB_URL').textContent);
+
+document.addEventListener('DOMContentLoaded',()=>{
+    console.log(backendUrl)
+})
